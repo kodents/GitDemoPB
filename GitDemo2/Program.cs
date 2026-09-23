1c3de6b13cc9323f27003c16b3bd91a94222881d
Daniil <daniilkodents@gmail.com>
2026-09-23 14:22:49 +0500

    добавил ридми

--- GitDemo2/Program.cs
+++ GitDemo2/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine(2+3);
+﻿var demo = 15;
+var text = "Изучаем C#";
+
+Console.WriteLine(text);
+
+var line = Console.ReadLine();
+Console.WriteLine(line);
+
+if (demo > 5)
+{
+    Console.WriteLine("demo больше 5");
+}
+else
+{
+    Console.WriteLine("demo меньше либо равен 5");
+}
